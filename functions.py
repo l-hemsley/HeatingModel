3cbe5f9a9a7b29db0430f7198aa067589a7855e9
Lizzy Hemsley <ehemsley@laas.fr>
2022-12-31 15:30:57 +0000

    added real parameters

--- functions.py
+++ functions.py
@@ -1,21 +1,21 @@
 import numpy as np
 
-def calculate_Qout_conductive(U,area_cell,T1,T2,length_cell):
-    Qout=U*area_cell*(T1-T2)/length_cell
+def calculate_Qout_conductive(THcond,area_cell,T1,T2,length_cell):
+    Qout=THcond*area_cell*(T1-T2)/length_cell
+    return Qout
+
+def calculate_Qout_convective(HTC,area_cell,T1,T2):
+    Qout=HTC*area_cell*(T1-T2)
     return Qout
 
 def calculate_Tchange(Qin,Qout,t_step,densityC,volume_cell):
     DT=(Qin-Qout)*t_step/(densityC*volume_cell)
     return DT
 
-def calculate_Qout_convective(HTC,area_cell,T1,T2):
-    Qout=HTC*area_cell*(T1-T2)
-    return Qout
-
 class output_surface_parameters:
     #class related to the input parameters on the DMD
-    def __init__(self,U,HTC,densityC,thickness,area,N_cells,initial_T):
-        self.U = U
+    def __init__(self,THcond,HTC,densityC,thickness,area,N_cells,initial_T):
+        self.THcond = THcond
         self.HTC= HTC
         self.densityC= densityC
         self.thickness =thickness
